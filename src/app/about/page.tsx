449fc2848bba12cab5f88b6cfb1ee4a69e828329
Anna <akuroshenkova@gmail.com>
2023-08-15 12:11:58 +0100

    nextjs class 1 and 2

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return(
+        <div>
+            <h2>About</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum magnam blanditiis deserunt optio possimus, quidem exercitationem eveniet provident iusto est aliquid iure reprehenderit dolorum mollitia suscipit et cupiditate dolor perferendis, dignissimos consequatur doloribus nemo adipisci? Nihil, voluptatem quibusdam, illo debitis labore aut quia voluptates sint, obcaecati sit earum! Dolore, itaque.</p>
+        </div>
+    )
+}
